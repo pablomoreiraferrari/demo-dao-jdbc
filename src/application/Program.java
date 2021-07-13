335f51e79b47f4842a14593b1139a0a32737ccf7
Pablo Raphael <pabloraphaelmoreira@outlook.com>
2021-07-13 11:55:18 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,9 @@ import model.entities.Seller;
 
 public class Program {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
@@ -33,6 +36,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gamail.com", new Date(), 4000.0,department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: Seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
+	
 
 }
